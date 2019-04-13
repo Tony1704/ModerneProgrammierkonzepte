d4ce73168279e46952ad92a75737057d74dd460e
Grimmig18 <benno.grimm@gmx.de>
2019-04-13 12:16:05 +0200

    git commit suicide

--- src/app/TestAlgorithms.java
+++ src/app/TestAlgorithms.java
@@ -13,7 +13,7 @@ public class TestAlgorithms {
     public static void runTest() {
         int[] arr = new int[6000];
         int search = 10;
-        final int iterations = 100000;
+        final int iterations = 1000;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
