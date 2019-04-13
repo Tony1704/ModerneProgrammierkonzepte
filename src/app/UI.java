5076cc0f3ce19feac130aef6195b8b243d8bcc2f
TristanMrb <trisemig@aol.com>
2019-04-13 18:12:14 +0200

    Update UI

--- src/app/UI.java
+++ src/app/UI.java
@@ -13,12 +13,15 @@ public class UI extends JFrame {
     public UI () {
         super("Weil ich das gesagt HABE!!!");
 
+        Long [] allTimes = new Long[24];
+        int i;
         ArrayList<Map<String, Long>> list;
         list = TestAlgorithms.runTest();
 
         Iterator arr = list.iterator();
 
         while (arr.hasNext()) {
+            i = 0;
             Map<String, Long> map = (Map) arr.next();
             Set schluessel = map.keySet();
 
@@ -26,10 +29,14 @@ public class UI extends JFrame {
 
             while (mapIterator.hasNext()) {
                 String str = (String) mapIterator.next();
-                Long l = (Long) map.get(str);
-                System.out.println(str + ": " + l/1000000 + "ms");
+                Long l = (Long) (map.get(str)/1000000);
+
+                allTimes[i] = l;
+
+                i++;
             }
         }
+
 
         this.setLayout(new BorderLayout());
 
@@ -48,6 +55,23 @@ public class UI extends JFrame {
         panel_top.add(btn_cancel);
 
         // Bottom JFrame
+        JLabel name = new JLabel("Name:");
+        JLabel max = new JLabel("Max Time:");
+        JLabel min = new JLabel("Min Time");
+        JLabel total = new JLabel("Total Time");
+
+        JLabel FunctionalLinearSearchIndex = new JLabel("Functional LinearSearch Index: ");
+        JLabel FunctionalLinearSearchList = new JLabel("Functional LinearSearch List: ");
+        JLabel MultithreadedLinearSearchIndex = new JLabel("Multithreaded LinearSearch Index: ");
+        JLabel MultithreadedLinearSearchList = new JLabel("Multithreaded LinearSearch List: ");
+        JLabel OOLinearSearchIndex = new JLabel("Object Oriented LinearSearch Index: ");
+        JLabel OOLinearSearchList = new JLabel("Object Oriented LinearSearch List: ");
+        JLabel OOBinarysearch = new JLabel("Object Oriented BinarySearch: ");
+
+        JLabel test = new JLabel(allTimes[0].toString());
+        JLabel test1 = new JLabel(allTimes[1].toString());
+        JLabel test2 = new JLabel(allTimes[2].toString());
+
         JPanel panel_bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         JPanel panel_bottom_search = new JPanel(new FlowLayout(FlowLayout.CENTER));
@@ -55,8 +79,24 @@ public class UI extends JFrame {
         JPanel panel_bottom_sort = new JPanel(new FlowLayout(FlowLayout.CENTER));
         panel_bottom_sort.setBorder(border_sort);
 
-        JPanel panel_search = new JPanel(new GridLayout(4, 2));
+        JPanel panel_search = new JPanel(new GridLayout(0, 4));
         JPanel panel_sort = new JPanel(new GridLayout(4, 2));
+
+        panel_search.add(name);
+        panel_search.add(max);
+        panel_search.add(min);
+        panel_search.add(total);
+        panel_search.add(FunctionalLinearSearchIndex);
+        panel_search.add(test);
+        panel_search.add(test1);
+        panel_search.add(test2);
+        panel_search.add(FunctionalLinearSearchList);
+        panel_search.add(MultithreadedLinearSearchIndex);
+        panel_search.add(MultithreadedLinearSearchList);
+        panel_search.add(OOLinearSearchIndex);
+        panel_search.add(OOLinearSearchList);
+        panel_search.add(OOBinarysearch);
+
 
         panel_bottom_search.add(panel_search);
         panel_bottom_search.add(panel_sort);
@@ -66,7 +106,7 @@ public class UI extends JFrame {
 
         // Configuration Frame
         this.add(panel_top, BorderLayout.NORTH);
-        this.add(panel_bottom, BorderLayout.SOUTH);
+        this.add(panel_bottom, BorderLayout.CENTER);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
