cb7a35616192fb4235f529a98dcf89f767452473
Grimmig18 <benno.grimm@gmx.de>
2019-04-13 15:47:44 +0200

    adhslhd

--- src/oo/quicksort/OOQuicksort.java
+++ src/oo/quicksort/OOQuicksort.java
@@ -1,20 +1,10 @@
 package oo.quicksort;
 
 public class OOQuicksort {
-    private static long bComparisons = 0;
-    private static long bArrayAccesses = 0;
-
-    private static long qComparisons = 0;
-    private static long qArrayAccesses = 0;
-    private static long qFunctionsCalls = 0;
-    private static long qdivideCalls = 0;
-
     public static void quickSort(int[] arr, int left, int right) {
         int divideIndex = divide(arr, left, right);
         if (divideIndex != -1 && left < right) {
-            qFunctionsCalls++;
             quickSort(arr, left, divideIndex - 1);
-            qFunctionsCalls++;
             quickSort(arr, divideIndex + 1, right);
         }
     }
@@ -22,14 +12,9 @@ public class OOQuicksort {
     public static void quickSort(int[] arr) {
         quickSort(arr, 0, arr.length - 1);
 
-        // System.out.println("Function Calls (Quick): " + qFunctionsCalls);
-        // System.out.println("Comparsions (Quick): " + qComparisons);
-        // System.out.println("Array Accesses (Quick): " + qArrayAccesses);
-        // System.out.println("Divide Calls (Quick): " + qdivideCalls);
     }
 
     public static int divide(int[] arr, int left, int right) {
-        qdivideCalls++;
         if (right < left) {
             return -1;
         }
@@ -37,36 +22,13 @@ public class OOQuicksort {
         int counter = left - 1;
 
         for (int i = left; i <= right - 1; i++) {
-            qComparisons++;
             if (arr[i] <= arr[right]) {
                 counter++;
-                qArrayAccesses++;
                 swap(arr, counter, i);
             }
         }
-
         swap(arr, (++counter), right);
         return counter;
-
-    }
-
-    // Bubble Sort for some testing
-    public static void bubbleSort(int[] arr) {
-        boolean hasChanged = true;
-        for (int i = 0; i < arr.length - 1 && hasChanged; i++) {
-            hasChanged = false;
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                bComparisons++;
-                if (arr[j] > arr[j + 1]) {
-                    bArrayAccesses++;
-                    swap(arr, j, j + 1);
-                    hasChanged = true;
-                }
-            }
-        }
-
-        // System.out.println("Comparsions (Bubble): " + bComparisons);
-        // System.out.println("Array Accesses (Bubble): " + bArrayAccesses);
 
     }
 
@@ -84,7 +46,7 @@ public class OOQuicksort {
                 return false;
             }
         }
-
         return true;
     }
+
 }
